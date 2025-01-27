45f526faf9f0f6bbd96b2ab64174c3f405671d38
Utsav Chouhan <144726026+utsav0092@users.noreply.github.com>
2025-01-27 18:29:33 +0530

    initial commit

--- My_Learning/2_Step/1_lec_sorting_one/2_GFG_selection.py
+++ My_Learning/2_Step/1_lec_sorting_one/2_GFG_selection.py
@@ -3,11 +3,11 @@
 def selectionSort(arr):
     n = len(arr)
     for i in range(n - 1): #start 0 to n-2
-        min_index = i # setting the first index as min
+        min_index = i # setting the first index as min not index value
         for j in range(i + 1, n): #start i+1 to n-1
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+            if arr[j] < arr[min_index]: #check the indexes
+                min_index = j #if found point to that index not index value
+        arr[i], arr[min_index] = arr[min_index], arr[i] #then swap with the index values
     return arr
 
 if __name__ == "__main__":
---
+++ My_Learning/2_Step/1_lec_sorting_one/3_GFG_insertion.py
@@ -0,0 +1,14 @@
+'''GFG solution for Insertion sort using python'''
+def insertion(arr):
+    n = len(arr)
+    for i in range(1, n): # 1 to n-1
+        j = i
+        while (j > 0 and arr[j-1] > arr[j]):
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+    return arr
+
+if __name__ == "__main__":
+    arr = list(map(int, input().split()))
+    print(f"Before sorting: {arr}")
+    print(f"After sorting: {insertion(arr)}")
