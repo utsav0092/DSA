fc8d953dca3a1fffa341a9aaf1e6efdcbc60d636
Utsav Chouhan <144726026+utsav0092@users.noreply.github.com>
2025-01-29 18:23:07 +0530

    initial commit

---
+++ My_Learning/3_step_Arrays/1_lec_easy/1_GFG_max_ele.py
@@ -0,0 +1,21 @@
+'''Find the max element in the array'''
+'''With sorting'''
+
+def findMax(arr):
+    arr.sort()
+    return arr[-1]
+
+arr = list(map(int, input().split()))
+print(f"Max Element is: {findMax(arr)}")
+
+'''Without using sort'''
+
+def findMax(arr):
+    m = arr[0]
+    for i in range(len(arr)):
+        if (arr[i] > m):
+            arr[i], m = m, arr[i]
+    return m
+
+arr = list(map(int, input().split()))
+print(f"Max Element is: {findMax(arr)}")
---
+++ My_Learning/3_step_Arrays/1_lec_easy/2_GFG_second_max.py
@@ -0,0 +1,13 @@
+'''Find the second max element in array'''
+'''with sort'''
+
+# def Smax(arr):
+#     arr.sort()
+#     return arr[-2]
+
+# arr = list(map(int, input().split()))
+# print(f"Element is: {Smax(arr)}")
+
+'''without sort'''
+
+# def Smax(arr):
