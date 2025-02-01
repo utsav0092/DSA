248cceed63d63cf181cd22185b0b070cb22cd9f5
Utsav Chouhan <144726026+utsav0092@users.noreply.github.com>
2025-02-01 22:43:03 +0530

    initial commit

--- My_Learning/3_step_Arrays/1_lec_easy/5_Leet_rotate_k.py
+++ My_Learning/3_step_Arrays/1_lec_easy/5_Leet_rotate_k.py
@@ -1,31 +1,32 @@
 '''Rotate array by one position'''
 '''TC = O(n) and SC = O(1)'''
 
-# def RotateByOne(arr):
-#     n = len(arr)
-#     temp = arr[0]
-#     for i in range(1, n):
-#         arr[i-1] = arr[i]
-#     arr[n-1] = temp
-#     return arr
+def RotateByOne(arr):
+    n = len(arr)
+    temp = arr[0]
+    for i in range(1, n):
+        arr[i-1] = arr[i]
+    arr[n-1] = temp
+    return arr
 
-# arr = [1, 2, 3, 4, 5]
-# print(RotateByOne(arr))
+arr = [1, 2, 3, 4, 5]
+print(RotateByOne(arr))
 
 '''Leetcode - Rotate an array by k position'''
 
-def RotateByK(arr, k):
-    j = 1
-    n = len(arr)
-    for i in range(k):
-        temp = arr[0]
-        while (j < n):
-            arr[j-1] = arr[j]
-            j += 1
-        arr[n-1] = temp
-    return arr
+class Solution:
+    def rotate(self, nums, k):
+        n = len(nums)
+        k = k % n  # Handle cases where k > n
+        # Reverse the entire array
+        nums.reverse() # [7, 6, 5, 4, 3, 2, 1] 
+        # Reverse the first k elements
+        nums[:k] = reversed(nums[:k]) # return reversed part [5, 6, 7, 4, 3, 2, 1]
+        # Reverse the remaining elements
+        nums[k:] = reversed(nums[k:]) # [5, 6, 7, 1, 2, 3, 4]
 
-    # [3, 4, 5, 1, 2]
-arr = [1, 2, 3, 4, 5]
-k = 2
-print(RotateByK(arr, k))
+nums = list(map(int, input("Enter the list values: ").split()))
+k = int(input("Enter the value: "))
+sol = Solution()
+sol.rotate(nums, k)
+print(nums) 
