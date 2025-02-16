be6bbc1b129545247746a5ca1c85d9b56230e0d7
Utsav Chouhan <144726026+utsav0092@users.noreply.github.com>
2025-02-16 23:34:01 +0530

    initial commit

--- My_Learning/3_step_Arrays/2_lec_medium/1_Leet_2sum.py
+++ My_Learning/3_step_Arrays/2_lec_medium/1_Leet_2sum.py
@@ -1,55 +1,63 @@
 '''Leetcode - 2 sum problem'''
 '''My Brute force : tc - O(n^2) and sc - O(1)'''
 
-# def twoSum(arr, k):
-#     n = len(arr)
-#     i = 0
-#     j = 0
-#     while (i < n):
-#         while (j < n):
-#             if arr[i] + arr[j] == k:
-#                 return arr[i], arr[j]
-#             else:
-#                 j += 1
-#         i += 1
-
-# def main():
-#     arr = [3,3,2,2,1]
-#     k = 6
-#     print(twoSum(arr, k))
-
-# if __name__ == "__main__":
-#     main()
-
-'''Striver's code Brute force'''
-'''tc - O(n^2) and sc - O(1)'''
-
-# def twoSum(arr, k):
-#     n = len(arr)
-#     for i in range(n):
-#         for j in range(n):
-#             if i == j:
-#                 continue
-#             if arr[i] + arr[j] == k:
-#                 return arr[i], arr[j]
-
-# def main():
-#     arr = [3,3,2,2,1]
-#     k = 6
-#     print(twoSum(arr, k))
-
-# if __name__ == "__main__":
-#     main()
-
-'''Better solution (hasing)'''
-
 def twoSum(arr, k):
     n = len(arr)
-    
+    i = 0
+    j = 0
+    while (i < n):
+        while (j < n):
+            if arr[i] + arr[j] == k:
+                return arr[i], arr[j]
+            else:
+                j += 1
+        i += 1
+
 def main():
     arr = [3,3,2,2,1]
     k = 6
     print(twoSum(arr, k))
+
+if __name__ == "__main__":
+    main()
+
+'''Striver's code Brute force'''
+'''tc - O(n^2) and sc - O(1)'''
+
+def twoSum(arr, k):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if arr[i] + arr[j] == k:
+                return arr[i], arr[j]
+
+def main():
+    arr = [3,3,2,2,1]
+    k = 6
+    print(twoSum(arr, k))
+
+if __name__ == "__main__":
+    main()
+
+'''Better solution (hasing)'''
+'''tc - O(n) and sc - O(n)'''
+
+def twoSum(nums, target):
+    num_map = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return -1
+    
+def main():
+    nums = [1,2,3,4,5]
+    target = 9
+    print(twoSum(nums, target))
 
 if __name__ == "__main__":
     main()
