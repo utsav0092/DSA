93164fddcdb6ceb65fbffdebae37b5be2a69cf75
Utsav Chouhan <144726026+utsav0092@users.noreply.github.com>
2025-03-16 22:43:02 +0530

    initial commit

---
+++ My_Learning/3_step_Arrays/2_lec_medium/14_Leet_countelesumK.py
@@ -0,0 +1,44 @@
+'''Leetcode - Count the number of subarrays whose sum equals K '''
+'''TC: O(n^2), SC: O(1)'''  
+'''Better solution'''
+
+# def sumEqual(arr, k):
+#     n = len(arr)
+#     count = 0
+
+#     for i in range(n):
+#         summ = 0  
+#         for j in range(i, n):
+#             summ += arr[j]
+#             if summ == k:
+#                 count += 1
+#                 print("Valid subarray:", arr[i:j+1]) 
+
+#     return count
+
+# def main():
+#     arr = [1, 2, 3, -3, 1, 1, 1, 4, 2, -3]  # Expected total subarrays: 8
+#     k = 3
+#     print("Total subarrays:", sumEqual(arr, k))
+
+# if __name__ == "__main__":
+#     main()
+
+'''--------------------------------------------------------------------------------------------------'''
+
+'''Optimised solution (prefix-sum / hashmap)'''
+'''tc - O(n) and sc - O(n)'''
+
+def sumEqual(arr, k):
+    n = len(arr)
+    hash_map = {0, 1}
+    total = 0
+    return total
+
+def main():
+    arr = [1, 2, 3, -3, 1, 1, 1, 4, 2, -3]  # Expected total subarrays: 8
+    k = 3
+    print("Total subarrays:", sumEqual(arr, k))
+
+if __name__ == "__main__":
+    main()
